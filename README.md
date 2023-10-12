be06bd25417bd630e791f80c8bc79c250f823ba0
Lucas Tannus <lucas.tannus@quizlet.com>
2023-10-11 22:46:49 -0300

    feat: add infra samples

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# terraform
-The goal of this repository it's apply terraform's knowledge 
+# Terraform
+The goal of this repository it's apply terraform's knowledge.
+
+## Setting AWS Provider
+AWS credentials can be provided by using the `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` environment variables. Also, the region can be set using the `AWS_REGION` environment variable.
+
+```
+export AWS_ACCESS_KEY_ID="anaccesskey"
+export AWS_SECRET_ACCESS_KEY="asecretkey"
+export AWS_REGION="us-west-2"
+```
+
+## Initializing Terraform module
+Run the following command in order to set all declared providers and initialize the terraform project:
+
+```
+terraform init
+```
+
+Run the following command to check if there is any possible change to be made:
+
+```
+terraform plan
+```
+
+Run the following command to apply all pending changes:
+
+```
+terraform apply
+```
+
+Run the following commando to undo all changed made:
+
+```
+terraform destroy
+```
---
+++ main.tf
@@ -0,0 +1,94 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAQALA7MCNN3VIQU4E"
+  secret_key = "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"
+}
+
+# Create a VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Main VPC"
+  }
+}
+
+# Create a Subnet
+resource "aws_subnet" "main_sn" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main Subnet"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "main_ig" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "Main IG"
+  }
+}
+
+# Create a Route Table
+resource "aws_default_route_table" "main_vpc_default_rt" {
+  default_route_table_id = aws_vpc.main_vpc.default_route_table_id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.main_ig.id
+  }
+
+  tags = {
+    Name = "Main VPC Default Route Table"
+  }
+}
+
+# Create a Security Group
+resource "aws_security_group" "main_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Default Security Group"
+  }
+}
+
+# Create an EC2
+resource "aws_instance" "main_ec2" {
+  ami                         = "ami-041feb57c611358bd"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.main_sg.id]
+  subnet_id                   = aws_subnet.main_sn.id
+  associate_public_ip_address = true
+  key_name                    = "production_ssh_key"
+
+  tags = {
+    Name = "Main EC2"
+  }
+}
